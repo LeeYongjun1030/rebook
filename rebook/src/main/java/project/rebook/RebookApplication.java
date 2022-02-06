9a4f9a7cf15020f5b3ef4f9e97f0edc5da027417
Lee_Yongjun <yiyj1030@gmail.com>
2022-02-06 22:19:57 +0900

    create project

---
+++ rebook/src/main/java/project/rebook/RebookApplication.java
@@ -0,0 +1,13 @@
+package project.rebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebookApplication.class, args);
+	}
+
+}
