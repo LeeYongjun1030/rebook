3e9aeb24a60346c6670b8ff1722929ec37ecba16
Lee_Yongjun <yiyj1030@gmail.com>
2022-10-11 20:49:13 +0900

    'test'

--- rebook/src/main/java/project/rebook/RebookApplication.java
+++ rebook/src/main/java/project/rebook/RebookApplication.java
@@ -3,9 +3,14 @@ package project.rebook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
 @SpringBootApplication
 public class RebookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RebookApplication.class, args);
+		//test
+
 	}
 }
