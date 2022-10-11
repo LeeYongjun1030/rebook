531def98f015ed6e27a5604cfd825890e42d0618
jjunibbuni <jjunibbuni@naver.com>
2022-10-11 20:55:46 +0900

    jjuni bbuni test

--- rebook/src/main/java/project/rebook/RebookApplication.java
+++ rebook/src/main/java/project/rebook/RebookApplication.java
@@ -10,6 +10,6 @@ import javax.persistence.EntityManagerFactory;
 public class RebookApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RebookApplication.class, args);
-		//testdfd
+		//testdfdccc
 	}
 }
